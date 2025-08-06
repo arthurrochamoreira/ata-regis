b8ff4f8e20d8887efdc2501191c00150d8c9b4ff
Arthur Rocha <123815111+arthurrochamoreira@users.noreply.github.com>
2025-08-06 02:58:48 -0300

    Revert "refactor: centralize color definitions"

--- src/ui/theme/shadows.py
+++ src/ui/theme/shadows.py
@@ -4,19 +4,19 @@ from . import colors
 SHADOW_XL = ft.BoxShadow(
     blur_radius=25,
     offset=ft.Offset(0, 20),
-    color=colors.SHADOW_COLOR,
+    color=colors.BLACK12,
 )
 
 SHADOW_LG = ft.BoxShadow(
     blur_radius=15,
     offset=ft.Offset(0, 10),
-    color=colors.SHADOW_COLOR,
+    color=colors.BLACK12,
 )
 
 SHADOW_MD = ft.BoxShadow(
     blur_radius=6,
     offset=ft.Offset(0, 4),
-    color=colors.SHADOW_COLOR,
+    color=colors.BLACK12,
 )
 
 __all__ = [
